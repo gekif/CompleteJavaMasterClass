491042d893dee371792d33efdfe50e8cc3f6bfc8
Gekif <dzulfikar.maulana@gmail.com>
2019-07-07 18:13:18 +0700

    4. Variables, Data Types, Operators: 2. What is Variable

---
+++ 4.VariableDataTypeOperator/Variable/src/Hello.java
@@ -0,0 +1,44 @@
+public class Hello {
+    public static void main(String[] args) {
+        /**
+         * My First Java Project
+         */
+        // Print Name
+//        System.out.println("Hello " + name);
+
+
+        /**
+         * Challenge
+         */
+        //        System.out.println("Hello " + name);
+
+//        String name = "Fikar";
+
+
+
+        /**
+         * Integer Variable
+         */
+//        int myFirstNumber = 5;
+//        int myFirstNumber = 10 + 5;
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+//        int myThirdNumber = 6;
+        int myThirdNumber = myFirstNumber * 2;
+        int myTotal =  myFirstNumber + mySecondNumber + myThirdNumber;
+
+//        System.out.println(myFirstNumber);
+//        System.out.println(mySecondNumber + myThirdNumber);
+//        System.out.println(myTotal);
+
+
+        /**
+         * Challenge
+         */
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
+
+    }
+}
