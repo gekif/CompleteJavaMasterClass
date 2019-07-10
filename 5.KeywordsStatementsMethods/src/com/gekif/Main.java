70497563c43f7efc3678fffa51f0b483f4710122
Gekif <dzulfikar.maulana@gmail.com>
2019-07-10 19:27:20 +0700

    5. Java Tutorial Expressions, Statements, Code blocks, Methods and more: Keywords and Expressions

---
+++ 5.KeywordsStatementsMethods/src/com/gekif/Main.java
@@ -0,0 +1,29 @@
+package com.gekif;
+
+public class Main {
+
+    public static void main(String[] args) {
+        // A mile is equal to 1.609344 kilometres
+        double kilometres = (100 * 1.609344);
+        int highScore = 50;
+
+        if (highScore == 50) {
+            System.out.println("This is an expression");
+        }
+
+
+        /*
+         * Challenge
+         * In the following code that i will type below, write down what parts of the code
+         * are expressions.
+         */
+
+        int score = 100;
+
+        if (score > 99) {
+            System.out.println("You've got the high score");
+            score = 0;
+        }
+
+    }
+}
