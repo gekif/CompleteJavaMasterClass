c86efebc782da717e00b98a831c85098f552a54d
Gekif <dzulfikar.maulana@gmail.com>
2019-08-13 08:59:35 +0700

    6. Control Flow Statements: Reading User Input Challenge

---
+++ 6.ControlFlowStatements/ReadingUserInputChallenge/src/com/gekif/Main.java
@@ -0,0 +1,40 @@
+package com.gekif;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+        int counter = 0;
+        int sum = 0;
+
+//        while (true) {
+        while (counter < 10) {
+            int order = counter + 1;
+            System.out.print("Enter number #" + order + ": ");
+
+            boolean isAnInt = scanner.hasNextInt();
+
+            if (isAnInt) {
+                int number = scanner.nextInt();
+                counter++;
+                sum += number;
+
+                /*if (counter == 10) {
+                    break;
+                }*/
+
+            } else {
+                System.out.println("Invalid Number");
+            }
+
+            scanner.nextLine(); // handle end of line (enter key)
+        }
+
+        System.out.println("The total number = " + sum);
+        scanner.close();
+    }
+}
