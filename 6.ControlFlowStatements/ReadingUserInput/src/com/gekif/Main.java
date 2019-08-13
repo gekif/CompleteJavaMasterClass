abbcb26f32c8854b711f0238c803eef23caf439c
Gekif <dzulfikar.maulana@gmail.com>
2019-08-13 08:45:47 +0700

    6. Control Flow Statements: Problem and Solution

--- 6.ControlFlowStatements/ReadingUserInput/src/com/gekif/Main.java
+++ 6.ControlFlowStatements/ReadingUserInput/src/com/gekif/Main.java
@@ -5,23 +5,43 @@ import java.util.Scanner;
 public class Main {
 
     public static void main(String[] args) {
+
         // User input object
         Scanner scanner = new Scanner(System.in);
 
+
         // Input Year of birth
         System.out.print("Enter your year of birth: ");
-        int yearOfBirth = scanner.nextInt();
-        scanner.nextLine(); // handle next line character (enter key)
 
-        // Input Name
-        System.out.print("Enter your name: ");
-        String name = scanner.nextLine();
+        // Check if the number is numeric or character
+        boolean hasNextInt = scanner.hasNextInt();
 
-        // To know your age
-        int age = 2019 - yearOfBirth;
+        if (hasNextInt) {
+            int yearOfBirth = scanner.nextInt();
+            scanner.nextLine(); // handle next line character (enter key)
 
-        // Output
-        System.out.println("Your name is " + name + ", and you are " + age + " years old");
+
+            // Input Name
+            System.out.print("Enter your name: ");
+            String name = scanner.nextLine();
+
+
+            // To know your age
+            int age = 2019 - yearOfBirth;
+
+
+            // Age Validation
+            if (age >= 0 && age <= 100) {
+
+                // Output
+                System.out.println("Your name is " + name + ", and you are " + age + " years old");
+            } else {
+                System.out.println("Invalid year of birth");
+            }
+
+        } else {
+            System.out.println("Unable to parse year of birth");
+        }
 
         scanner.close();
     }
