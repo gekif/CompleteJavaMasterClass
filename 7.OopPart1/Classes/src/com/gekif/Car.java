6271931e9563f3bd2cb164d913e2df42466b5c56
Gekif <dzulfikar.maulana@gmail.com>
2019-08-14 08:08:58 +0700

    7. OOP Part 1: Classes Part 1

---
+++ 7.OopPart1/Classes/src/com/gekif/Car.java
@@ -0,0 +1,11 @@
+package com.gekif;
+
+public class Car {
+
+    private int doors;
+    private int wheels;
+    public String model;
+    private String colour;
+
+
+}
---
+++ 7.OopPart1/Classes/src/com/gekif/Main.java
@@ -0,0 +1,13 @@
+package com.gekif;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Car porsche = new Car();
+        Car holden = new Car();
+
+        porsche.model = "Green";
+        System.out.println(porsche.model);
+
+    }
+}
