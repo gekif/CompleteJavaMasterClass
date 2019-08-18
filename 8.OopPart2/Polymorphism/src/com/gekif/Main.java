9c1e80f747394dcc4190c24faad84982206f46bb
Gekif <dzulfikar.maulana@gmail.com>
2019-08-18 14:37:38 +0700

    8. OOP Part 2: Polymorphism

--- 8.OopPart2/Polymorphism/src/com/gekif/Main.java
+++ 8.OopPart2/Polymorphism/src/com/gekif/Main.java
@@ -1,8 +1,128 @@
 package com.gekif;
 
+
+/**
+ * Base Class
+ */
+class Movie {
+
+    private String name;
+
+    public Movie(String name) {
+        this.name = name;
+    }
+
+    public String plot() {
+        return "No Plot Here";
+    }
+
+    public String getName() {
+        return name;
+    }
+}
+
+
+/**
+ * List of All Movies Extends From Movies
+ * Total 5 Classes
+ */
+class Jaws extends Movie {
+
+    public Jaws() {
+        super("Jaws");
+    }
+
+    public String plot() {
+        return "A shark eats lots of people";
+    }
+}
+
+
+class IndependenceDay extends Movie {
+
+    public IndependenceDay() {
+        super("Independence Day");
+    }
+
+    @Override
+    public String plot() {
+        return "Aliens attempt to take over planet earth";
+    }
+}
+
+
+class MazeRunner extends Movie {
+
+    public MazeRunner() {
+        super("Maze Runner");
+    }
+
+    @Override
+    public String plot() {
+        return "Kids try and escape a maze.";
+    }
+}
+
+
+class StarWars extends Movie {
+
+    public StarWars() {
+        super("Star Wars");
+    }
+
+    @Override
+    public String plot() {
+        return "Imperial forces try to take over the universe";
+    }
+}
+
+
+class Forgetable extends Movie {
+    public Forgetable() {
+        super("Forgetable");
+    }
+
+    // No Plot
+}
+
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        for (int i = 1; i < 11; i++) {
+            Movie movie = randomMovie();
+            System.out.println("Movie #" + i +
+                    " : " + movie.getName() + "\n" +
+                    "Plot: " + movie.plot() + "\n");
+        }
     }
+
+
+    /**
+     * Return Random Movie
+     */
+
+    public static Movie randomMovie() {
+
+        int randonNumber = (int) (Math.random() * 5) + 1;
+        System.out.println("Random number generates was " + randonNumber);
+
+        switch (randonNumber) {
+            case 1:
+                return new Jaws();
+            case 2:
+                return new IndependenceDay();
+            case 3:
+                return new MazeRunner();
+            case 4:
+                return new StarWars();
+            case 5:
+                return new Forgetable();
+        }
+
+        return null;
+    }
+
+
 }
