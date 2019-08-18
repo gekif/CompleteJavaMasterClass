9374a42f6787a1f3ae589af7b28b33272cf3cd45
Gekif <dzulfikar.maulana@gmail.com>
2019-08-18 15:43:31 +0700

    8. OOP Part 2: OOP Master Challenge Part 1

--- 8.OopPart2/OOPTest/src/com/gekif/Main.java
+++ 8.OopPart2/OOPTest/src/com/gekif/Main.java
@@ -3,6 +3,16 @@ package com.gekif;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Hamburger hamburger = new Hamburger("Basic", "Sausage", 3.56, "White");
+        double price = hamburger.itemizeHamburger();
+
+        hamburger.addHamburgerAddition1("Tomato", 0.27);
+        hamburger.addHamburgerAddition2("Lettuce", 0.75);
+        hamburger.addHamburgerAddition3("Cheese", 1.13);
+
+//        price = hamburger.itemizeHamburger();
+        System.out.println("The Total Burger Price is " + hamburger.itemizeHamburger());
+
     }
 }
